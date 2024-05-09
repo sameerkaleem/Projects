be088896d2a8fbf7d67545ee5f866510535ba387
sameerkaleem <160517211+sameerkaleem@users.noreply.github.com>
2024-05-10 01:03:03 +0500

    Currency Converter
    A simple currency converter with typescript.

---
+++ my_app_currency_converter/main.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let currency = {
+    USD: 1,
+    GBP: 0.79,
+    EUR: 0.92,
+    PKR: 277.98,
+};
+let ans = await inquirer.prompt([
+    {
+        name: "from",
+        type: "list",
+        message: "enter to currency",
+        choices: ["USD", "GBP", "EUR", "PKR"],
+    },
+    {
+        name: "to",
+        type: "list",
+        message: "enter to currency",
+        choices: ["USD", "GBP", "EUR", "PKR"],
+    },
+    {
+        name: "amount",
+        type: "number",
+        message: "enter amount",
+    },
+]);
+let from = currency[ans.from];
+let to = currency[ans.to];
+let amount = ans.amount;
+let base_Amount = amount / from;
+let converted_Amount = base_Amount * to;
+console.log(converted_Amount);

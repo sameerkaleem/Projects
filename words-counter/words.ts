5041daf623afa3fb0a5a0946a2ccf188b7bfc2bb
sameerkaleem <160517211+sameerkaleem@users.noreply.github.com>
2024-05-10 01:06:52 +0500

    Words Counter
    A Simple Words Counter with typescript.

---
+++ words-counter/words.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer"
+
+let spell = await inquirer.prompt 
+
+(
+    [
+        {
+
+                name:"words",
+                type:"input",
+                message:"Enter words to count"
+
+        }
+    ]
+)
+
+let words = spell.words.trim().split(" ")
+
+console.log(words.length)
